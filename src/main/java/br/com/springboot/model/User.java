f7d6a4403165fbf5c80f40fdf7cd2c41c65e4093
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-01-15 15:33:53 -0300

    Aulas de Spring boot

---
+++ src/main/java/br/com/springboot/model/User.java
@@ -0,0 +1,20 @@
+package br.com.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+
+    private int id;
+    private String name;
+    private String userName;
+    private int age;
+    
+}
