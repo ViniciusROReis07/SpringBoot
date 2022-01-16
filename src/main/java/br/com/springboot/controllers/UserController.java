bddc70828bdfb61c788f946bce3099804a4f0891
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-01-16 15:15:12 -0300

    feat: Integração com o banco

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -1,5 +1,6 @@
 package br.com.springboot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 import br.com.springboot.model.User;
+import br.com.springboot.repository.UserRepository;
 
 @RestController
 @RequestMapping("/user")
@@ -19,10 +21,13 @@ public class UserController {
 
     private List<User> users = new ArrayList<>();
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/{id}")
     public User user(@PathVariable("id") int id) {
 
-        Optional<User> userFind = users.stream().filter(user -> user.getId() == id).findFirst();
+        Optional<User> userFind = userRepository.findById(id);
 
         if (userFind.isPresent()) {
             return userFind.get();
@@ -33,13 +38,12 @@ public class UserController {
 
     @PostMapping("/")
     public User register(@RequestBody User user) {
-        this.users.add(user);
-        return user;
+        return this.userRepository.save(user);
     }
 
     @GetMapping("/list")
     public List<User> list() {
-        return this.users;
+        return this.userRepository.findAll();
     }
 
 }
