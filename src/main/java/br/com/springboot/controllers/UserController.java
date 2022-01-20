3077317dda15232a1f28ee90052424b147fd53bc
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-01-19 22:10:57 -0300

    fix: Exclusão de usuário

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 
 import br.com.springboot.model.User;
 import br.com.springboot.repository.UserRepository;
@@ -58,14 +58,21 @@ public class UserController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable("id") int id) {
+    public String deleteUser(@PathVariable("id") int id) {
 
         Optional<User> userFind = userRepository.findById(id);
 
         if (userFind.isPresent()) {
             this.userRepository.deleteById(id);
+            return "Usuario " + id + " excluido com sucesso";
         }
-  
-        return ResponseEntity.noContent().build();
+
+        return "Não foi possivel excluir o usuário " + id;
+    }
+
+    @PutMapping("/alter")
+    public User alterUser(@RequestBody User user) {
+
+        return this.userRepository.save(user);
     }
 }
