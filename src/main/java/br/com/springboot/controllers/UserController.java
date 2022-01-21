b7592845b24bfdfd8398022711a131098409d33d
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-01-20 22:24:43 -0300

    fix: Put

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -1,6 +1,8 @@
 package br.com.springboot.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +29,7 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public User user(@PathVariable("id") int id) {
 
         Optional<User> userFind = userRepository.findById(id);
@@ -38,26 +42,31 @@ public class UserController {
     }
 
     @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
     public User register(@RequestBody User user) {
         return this.userRepository.save(user);
     }
 
     @GetMapping("/list")
+    @ResponseStatus(HttpStatus.OK)
     public List<User> list() {
         return this.userRepository.findAll();
     }
 
     @GetMapping("/list/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public List<User> listMore(@PathVariable("id") int id) {
         return this.userRepository.findByIdGreaterThan(id);
     }
 
     @GetMapping("/findByName/{name}")
+    @ResponseStatus(HttpStatus.OK)
     public List<User> findByName(@PathVariable("name") String name) {
         return this.userRepository.findByNameIgnoreCase(name);
     }
 
     @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public String deleteUser(@PathVariable("id") int id) {
 
         Optional<User> userFind = userRepository.findById(id);
@@ -70,9 +79,13 @@ public class UserController {
         return "Não foi possivel excluir o usuário " + id;
     }
 
-    @PutMapping("/alter")
-    public User alterUser(@RequestBody User user) {
+    @PutMapping("/alter/{id}")
+    public User alterUser(@PathVariable("id") int id, @RequestBody User user) {
 
-        return this.userRepository.save(user);
+        User userCurrent = this.userRepository.findById(id).get();
+
+        BeanUtils.copyProperties(user, userCurrent, "id");
+
+        return this.userRepository.save(userCurrent);
     }
 }
